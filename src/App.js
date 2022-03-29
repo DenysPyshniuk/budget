e9592850e28747ebde4e38e9936172fddd1b3f83
Devronder <devronder@gmail.com>
2022-03-29 11:36:12 -0700

    Create and use EntryLine component

--- src/App.js
+++ src/App.js
@@ -1,65 +1,27 @@
 
-import {Container, Grid, Segment, Icon} from 'semantic-ui-react';
+import {Container} from 'semantic-ui-react';
 import './App.css';
 import MainHeader from './components/MainHeader';
 import NewEntryForm from './components/NewEntryForm';
 import DisplayBalance from './components/DisplayBalance';
 import DisplayBalances from './components/DisplayBalances';
+import EntryLine from './components/EntryLine';
 
 function App() {
   return (
     <Container>
       <MainHeader title='Budget' />
-
       <DisplayBalance size='small' title='Your Balance:' value='2,550.53' />
-
       <DisplayBalances />
+      <MainHeader type='h3' title='History' />
 
-      <MainHeader type='h3' title='History'/>
-
-      <Segment color='red'>
-        <Grid columns={3} textAlign='right'>
-          <Grid.Row>
-          <Grid.Column width={10} textAlign='left'>Meat</Grid.Column>
-          <Grid.Column width={3}>$10,00</Grid.Column>
-            <Grid.Column width={3}>
-              <Icon name='edit' bordered></Icon>
-              <Icon name='trash' bordered></Icon>
-          </Grid.Column>
-          </Grid.Row>
-        </Grid>
-      </Segment>
-
-      <Segment color='green'>
-        <Grid columns={3} textAlign='right'>
-          <Grid.Row>
-          <Grid.Column width={10} textAlign='left'>Deposit</Grid.Column>
-          <Grid.Column width={3}>$125,00</Grid.Column>
-            <Grid.Column width={3}>
-              <Icon name='edit' bordered></Icon>
-              <Icon name='trash' bordered></Icon>
-          </Grid.Column>
-          </Grid.Row>
-        </Grid>
-      </Segment>
-
-      <Segment color='red'>
-        <Grid columns={3} textAlign='right'>
-          <Grid.Row>
-          <Grid.Column width={10} textAlign='left'>Coffee</Grid.Column>
-          <Grid.Column width={3}>$5,00</Grid.Column>
-            <Grid.Column width={3}>
-              <Icon name='edit' bordered></Icon>
-              <Icon name='trash' bordered></Icon>
-          </Grid.Column>
-          </Grid.Row>
-        </Grid>
-      </Segment>
+      <EntryLine description='Meat' value='10,00' isExpense/>
+      <EntryLine description='Deposit' value='125,00' />
+      <EntryLine description='Expense' value='125,00' isExpense />
+      <EntryLine description='Coffee' value='5,00' isExpense/>
 
       <MainHeader type='h3' title='Add new transaction'/>
-
       <NewEntryForm />
-
     </Container>
   );
 }
