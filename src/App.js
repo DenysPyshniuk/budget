805cc107774ddf557cb32ecee2a6acd8e239f79c
Devronder <devronder@gmail.com>
2022-03-28 21:04:31 -0700

    Add Grid

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 
-import {Container, Header, Statistic} from 'semantic-ui-react';
+import {Container, Header, Statistic, Grid, Segment} from 'semantic-ui-react';
 import './App.css';
 
 function App() {
@@ -10,6 +10,24 @@ function App() {
         <Statistic.Label>Your Balance:</Statistic.Label>
         <Statistic.Value>2,550.53</Statistic.Value>
       </Statistic>
+      <Segment textAlign='center'>
+        <Grid columns={2} divided>
+          <Grid.Row>
+            <Grid.Column>
+              <Statistic size='tiny' color='green'>
+                <Statistic.Label style={{textAlign:'left'}}>Income:</Statistic.Label>
+                <Statistic.Value>1,045.50</Statistic.Value>
+              </Statistic>
+            </Grid.Column>
+            <Grid.Column>
+              <Statistic size='tiny' color='red'>
+                <Statistic.Label style={{textAlign:'left'}}>Expenses:</Statistic.Label>
+                <Statistic.Value>623.50</Statistic.Value>
+              </Statistic>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Segment>
     </Container>
   );
 }
