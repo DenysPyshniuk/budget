f87635365abb5a2939b6e1f1a0049f1b3c758df6
Devronder <devronder@gmail.com>
2022-03-31 22:28:18 -0700

    Update values

--- src/App.js
+++ src/App.js
@@ -1,44 +1,60 @@
-import { useState, useEffect } from 'react';
-import {Container} from 'semantic-ui-react';
-import './App.css';
-import MainHeader from './components/MainHeader';
-import NewEntryForm from './components/NewEntryForm';
-import DisplayBalance from './components/DisplayBalance';
-import DisplayBalances from './components/DisplayBalances';
-import EntryLines from './components/EntryLines';
-import ModalEdit from './components/ModalEdit';
+import { useState, useEffect } from "react";
+import { Container } from "semantic-ui-react";
+import "./App.css";
+import MainHeader from "./components/MainHeader";
+import NewEntryForm from "./components/NewEntryForm";
+import DisplayBalance from "./components/DisplayBalance";
+import DisplayBalances from "./components/DisplayBalances";
+import EntryLines from "./components/EntryLines";
+import ModalEdit from "./components/ModalEdit";
 
 function App() {
   const [entries, setEntries] = useState(initialEntries);
-  const [description, setDescription] = useState('');
-  const [value, setValue] = useState('');
+  const [description, setDescription] = useState("");
+  const [value, setValue] = useState("");
   const [isExpense, setIsExpense] = useState(true);
   const [isOpen, setIsOpen] = useState(false);
   const [entryId, setEntryId] = useState();
+  const [incomeTotal, setIncomeTotal] = useState(0);
+  const [expenseTotal, setExpenseTotal] = useState(0);
+  const [total, setTotal] = useState(0);
 
   useEffect(() => {
-    if(!isOpen && entryId) {
-      const index = entries.findIndex(entry => entry.id === entryId);
+    if (!isOpen && entryId) {
+      const index = entries.findIndex((entry) => entry.id === entryId);
       const newEntries = [...entries];
       newEntries[index].description = description;
       newEntries[index].value = value;
       newEntries[index].isExpense = isExpense;
       setEntries(newEntries);
       resetEntry();
-
     }
-  }, [isOpen])
+  }, [isOpen]);
 
+  useEffect(() => {
+    let totalIncomes = 0;
+    let totalExpenses = 0;
+    entries.map((entry) => {
+      if (entry.isExpense) {
+        return (totalExpenses += Number(entry.value));
+      }
+      return (totalIncomes += Number(entry.value));
+    });
+
+    setTotal(totalIncomes - totalExpenses);
+    setExpenseTotal(totalExpenses);
+    setIncomeTotal(totalIncomes);
+  }, [entries]);
 
   function deleteEntry(id) {
-    const result = entries.filter(entry => entry.id !== id);
+    const result = entries.filter((entry) => entry.id !== id);
     setEntries(result);
   }
 
   function editEntry(id) {
     console.log(`edit entry with id ${id}`);
-    if(id) {
-      const index = entries.findIndex(entry => entry.id === id);
+    if (id) {
+      const index = entries.findIndex((entry) => entry.id === id);
       const entry = entries[index];
       setEntryId(id);
       setDescription(entry.description);
@@ -53,7 +69,7 @@ function App() {
       id: entries.lenght + 1,
       description,
       value,
-      isExpense
+      isExpense,
     });
     setEntries(result);
     resetEntry();
@@ -68,8 +84,8 @@ function App() {
   return (
     <Container>
       <MainHeader title="Budget" />
-      <DisplayBalance size="small" title="Your Balance:" value="2,550.53" />
-      <DisplayBalances />
+      <DisplayBalance size="small" title="Your Balance:" value={total} />
+      <DisplayBalances incomeTotal={incomeTotal} expenseTotal={expenseTotal}/>
       <MainHeader type="h3" title="History" />
 
       <EntryLines
@@ -110,43 +126,43 @@ const initialEntries = [
   {
     id: 1,
     description: "Work Income",
-    value: "$1000,00",
-    isExpense: false
+    value: 1000.00,
+    isExpense: false,
   },
   {
     id: 2,
     description: "Water bill",
-    value: "$20,00",
-    isExpense: true
+    value: 20.00,
+    isExpense: true,
   },
   {
     id: 3,
     description: "Rent",
-    value: "$300,00",
-    isExpense: true
+    value: 300.00,
+    isExpense: true,
   },
   {
     id: 4,
     description: "Power bill",
-    value: "$50,00",
-    isExpense: true
+    value: 50.00,
+    isExpense: true,
   },
   {
     id: 5,
     description: "Meat",
-    value: "$10,00",
-    isExpense: true
+    value: 10.00,
+    isExpense: true,
   },
   {
     id: 6,
     description: "Deposit",
-    value: "$125,00",
-    isExpense: false
+    value: 125.00,
+    isExpense: false,
   },
   {
     id: 7,
     description: "Coffee",
-    value: "$5,00",
-    isExpense: true
+    value: 5.00,
+    isExpense: true,
   },
-]
+];
--- src/components/DisplayBalances.js
+++ src/components/DisplayBalances.js
@@ -2,18 +2,18 @@ import React from 'react';
 import {Segment, Grid} from 'semantic-ui-react';
 import DispalyBalance from './DisplayBalance';
 
-function DisplayBalances() {
+function DisplayBalances({ incomeTotal, expenseTotal}) {
   return (
     <div>
       <Segment textAlign='center'>
         <Grid columns={2} divided>
           <Grid.Row>
             <Grid.Column>
-              <DispalyBalance size='tiny' color='green' style={{textAlign:'left'}} title='Income:'
-                value='1,045.50' />
+              <DispalyBalance size='tiny' color='green' style={{textAlign: 'left'}} title='Income:'
+                value={incomeTotal} />
             </Grid.Column>
             <Grid.Column>
-              <DispalyBalance size='tiny' color='red' style={{textAlign:'left'}} title='Expenses:' value='623.50' />
+              <DispalyBalance size='tiny' color='red' style={{textAlign:'left'}} title='Expenses:' value={expenseTotal} />
             </Grid.Column>
           </Grid.Row>
         </Grid>
