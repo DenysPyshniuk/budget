7e9ff7888286cff2f65ac5c93a4d0165d3bd3159
Denys Pyshniuk <denys.pyshniuk@gmail.com>
2022-04-05 00:00:36 -0700

    Pass modalsReducer to the store

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,12 +1,14 @@
 
 import {createStore, combineReducers} from 'redux';
 import { composeWithDevTools } from "@redux-devtools/extension";
-import entriesReducer from '../reducers/entries.reducers'
+import entriesReducer from '../reducers/entries.reducers';
+import modalsReducer from '../reducers/entries.reducers'
 
 const configureStore = () => {
   return createStore(
     combineReducers({
       entries: entriesReducer,
+      modals: modalsReducer,
     }), composeWithDevTools()
   );
 };
