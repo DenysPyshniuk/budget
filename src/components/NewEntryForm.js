a4c634c1e489d6618eea8780e19c7feed3f8683c
Denys Pyshniuk <denys.pyshniuk@gmail.com>
2022-04-04 22:53:19 -0700

    Use useDispatch for addEntry

--- src/components/NewEntryForm.js
+++ src/components/NewEntryForm.js
@@ -2,16 +2,27 @@ import React from 'react';
 import { Form } from 'semantic-ui-react';
 import ButtonSaveOrCancel from './ButtonSaveOrCancel';
 import EntryForm from './EntryForm';
+import {useState} from 'react';
+import { addEntryRedux } from '../actions/entries.actions';
+import { useDispatch } from 'react-redux';
 
-function NewEntryForm({
-  addEntry,
-  description,
-  value,
-  isExpense,
-  setDescription,
-  setValue,
-  setIsExpense
-}) {
+function NewEntryForm() {
+  const [description, setDescription] = useState("");
+  const [value, setValue] = useState("");
+  const [isExpense, setIsExpense] = useState(true);
+  const dispatch = useDispatch();
+
+  function addEntry() {
+    dispatch(addEntryRedux({
+      id: 9,
+      description,
+      value,
+      isExpense
+    }))
+    setDescription('');
+    setValue('');
+    setIsExpense(true);
+  }
 
   return (
     <Form untrackable="true">
