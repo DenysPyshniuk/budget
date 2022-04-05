c6d5d90243bc2018db7bc9433d9b127a2a6ac4bd
Denys Pyshniuk <denys.pyshniuk@gmail.com>
2022-04-04 22:34:17 -0700

    Use dispatch from redux to remove entry

--- src/components/EntryLine.js
+++ src/components/EntryLine.js
@@ -1,7 +1,10 @@
 import React, {Fragment} from 'react';
-import { Grid, Segment, Icon } from 'semantic-ui-react';
+import {Grid, Segment, Icon} from 'semantic-ui-react';
+import {useDispatch} from 'react-redux';
+import { removeEntryRedux, addEntryRedux } from "../actions/entries.actions";
 
-function EntryLine({id, description, value, isExpense = false, deleteEntry, editEntry}) {
+function EntryLine({id, description, value, isExpense = false, editEntry}) {
+  const dispatch = useDispatch();
 
   return (
     <Fragment>
@@ -11,8 +14,10 @@ function EntryLine({id, description, value, isExpense = false, deleteEntry, edit
               <Grid.Column width={10} textAlign='left'>{description}</Grid.Column>
               <Grid.Column width={3}>${value}</Grid.Column>
               <Grid.Column width={3}>
-                  <Icon name='edit' bordered onClick={() => editEntry(id)}></Icon>
-                  <Icon name='trash' bordered onClick={() => deleteEntry(id)}></Icon>
+              <Icon name='edit' bordered
+                onClick={() => editEntry(id)}></Icon>
+              <Icon name='trash' bordered
+                onClick={() => dispatch(removeEntryRedux(id))}></Icon>
               </Grid.Column>
               </Grid.Row>
             </Grid>
