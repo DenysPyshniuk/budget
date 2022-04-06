46c038347075aa73290884881334435d2b6e5ffb
Denys Pyshniuk <denys.pyshniuk@gmail.com>
2022-04-06 00:24:03 -0700

    Fix store for modals

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -2,7 +2,7 @@
 import {createStore, combineReducers} from 'redux';
 import { composeWithDevTools } from "@redux-devtools/extension";
 import entriesReducer from '../reducers/entries.reducers';
-import modalsReducer from '../reducers/entries.reducers'
+import modalsReducer from '../reducers/modals.reducers'
 
 const configureStore = () => {
   return createStore(
