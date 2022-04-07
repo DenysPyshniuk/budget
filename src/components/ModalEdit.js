0658d273f70b5a732990a4089046893bbc58ce6c
Denys Pyshniuk <denys.pyshniuk@gmail.com>
2022-04-06 23:43:18 -0700

    Update useEntryDetails hook

--- src/components/ModalEdit.js
+++ src/components/ModalEdit.js
@@ -2,7 +2,8 @@ import React from "react";
 import { Modal, Button } from "semantic-ui-react";
 import EntryForm from "./EntryForm";
 import {useDispatch} from 'react-redux';
-import {closeEditModal} from '../actions/modals.actions'
+import {closeEditModal} from '../actions/modals.actions';
+import useEntryDetails from './../hooks/useEntryDetails';
 
 
 function ModalEdit({
@@ -10,22 +11,20 @@ function ModalEdit({
   description,
   value,
   isExpense,
-  setDescription,
-  setValue,
-  setIsExpense,
 }) {
   const dispatch = useDispatch();
+  const entryUpdate = useEntryDetails(description, value, isExpense);
   return (
     <Modal open={isOpen}>
       <Modal.Header>Edit entry</Modal.Header>
       <Modal.Content>
         <EntryForm
-          description={description}
-          value={value}
-          isExpense={isExpense}
-          setDescription={setDescription}
-          setValue={setValue}
-          setIsExpense={setIsExpense}
+          description={entryUpdate.description}
+          value={entryUpdate.value}
+          isExpense={entryUpdate.isExpense}
+          setDescription={entryUpdate.setDescription}
+          setValue={entryUpdate.setValue}
+          setIsExpense={entryUpdate.setIsExpense}
         />
       </Modal.Content>
       <Modal.Actions>
