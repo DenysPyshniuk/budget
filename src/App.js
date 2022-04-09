a5441b821ef84de4daccc6878a605827e648e393
Denys Pyshniuk <denys.pyshniuk@gmail.com>
2022-04-09 00:21:27 -0700

    Use axios to fetch data and refactor

--- src/App.js
+++ src/App.js
@@ -7,19 +7,19 @@ import DisplayBalance from "./components/DisplayBalance";
 import DisplayBalances from "./components/DisplayBalances";
 import EntryLines from "./components/EntryLines";
 import ModalEdit from "./components/ModalEdit";
-import {useSelector} from 'react-redux';
-
+import { useSelector } from "react-redux";
+import axios from "axios";
 
 function App() {
   const [incomeTotal, setIncomeTotal] = useState(0);
   const [expenseTotal, setExpenseTotal] = useState(0);
   const [total, setTotal] = useState(0);
   const [entry, setEntry] = useState();
-  const {isOpen, id} = useSelector(state => state.modals);
-  const entries = useSelector(state => state.entries);
+  const { isOpen, id } = useSelector((state) => state.modals);
+  const entries = useSelector((state) => state.entries);
 
   useEffect(() => {
-    const index = entries.findIndex(entry => entry.id === id)
+    const index = entries.findIndex((entry) => entry.id === id);
     setEntry(entries[index]);
   }, [isOpen, id, entries]);
 
@@ -27,7 +27,7 @@ function App() {
     let totalIncomes = 0;
     let totalExpenses = 0;
     entries.map((entry) => {
-      if(entry.isExpense) {
+      if (entry.isExpense) {
         return (totalExpenses += Number(entry.value));
       }
       return (totalIncomes += Number(entry.value));
@@ -38,21 +38,27 @@ function App() {
     setIncomeTotal(totalIncomes);
   }, [entries]);
 
+  async function fetchInitialData() {
+    const result = await axios.get('http://localhost:3001/entries');
+    console.log(result);
+
+  }
+  useEffect(() => {
+    fetchInitialData();
+  }, []);
+
   return (
     <Container>
       <MainHeader title="Budget" />
       <DisplayBalance size="small" title="Your Balance:" value={total} />
-      <DisplayBalances incomeTotal={incomeTotal} expenseTotal={expenseTotal}/>
+      <DisplayBalances incomeTotal={incomeTotal} expenseTotal={expenseTotal} />
       <MainHeader type="h3" title="History" />
 
       <EntryLines entries={entries} />
 
       <MainHeader type="h3" title="Add new transaction" />
       <NewEntryForm />
-      <ModalEdit
-        isOpen={isOpen}
-        {...entry}
-      />
+      <ModalEdit isOpen={isOpen} {...entry} />
     </Container>
   );
 }
