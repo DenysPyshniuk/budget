534dc44cadf89efbb904110db85350b889a02dd6
Denys Pyshniuk <denys.pyshniuk@gmail.com>
2022-04-10 00:26:32 -0700

    Finish implementing getAllEntries function

--- src/sagas/entriesSaga.js
+++ src/sagas/entriesSaga.js
@@ -1,6 +1,10 @@
-import { take } from "redux-saga/effects";
+import { take, call } from "redux-saga/effects";
+import entriesTypes from '../actions/entries.actions';
+import axios from 'axios';
 
-
-function* getAllEntries() {
-  yield take('')
+export function* getAllEntries() {
+  yield take(entriesTypes.GET_ENTRIES);
+  console.log('where is entries');
+  const result = yield call(axios, 'http://localhost:3001/entries/');
+  console.log('result', result);
 }
