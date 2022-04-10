32c41f244dbc3e5a1eaa968881d0ee85bfb50166
Denys Pyshniuk <denys.pyshniuk@gmail.com>
2022-04-10 14:07:35 -0700

    Test a fork effect from saga

--- src/sagas/testSaga.js
+++ src/sagas/testSaga.js
@@ -1,27 +1,45 @@
-import {take, delay, put, call} from 'redux-saga/effects';
+import {take, delay, put, call, fork} from 'redux-saga/effects';
 
 function double(number) {
   return number * 2;
 }
 
-// export function* testSaga() {
-//   while(true) {
-//     console.log("Starting saga");
-//     const state = yield take('TEST_MESSAGE');
-//     const a = yield call(double, 2);
-//     console.log(a);
-//     const b = yield double(3);
-//     console.log(b);
-//     console.log("Finish saga", state);
-//   }
-// };
+export function* testSaga() {
+  while(true) {
+    console.log("Starting saga");
+    const state = yield take('TEST_MESSAGE');
+    const a = yield call(double, 2);
+    console.log(a);
+    const b = yield double(3);
+    console.log(b);
+    console.log("Finish saga", state);
+  }
+};
 
-// export function* dispatchTest() {
-//   while(true) {
-//     yield delay(1000);
-//     yield put({
-//       type: 'TEST_MESSAGE',
-//       payload: 1000
-//     });
-//   }
-// }
+function* doNothing() {
+  console.log('I have been called');
+  // yield delay(1000);
+  yield delay(3000);
+  console.log('I am doing nothing');
+  // console.log('I am doing nothing');
+  // console.log('I am doing nothing');
+}
+
+export function* testSagaFork() {
+  while (true) {
+    yield take('TEST_MESSAGE_2')
+    yield fork(doNothing)
+    yield fork(doNothing)
+    yield fork(doNothing)
+  }
+}
+
+export function* dispatchTest() {
+  while(true) {
+    yield delay(5000);
+    yield put({
+      type: 'TEST_MESSAGE_2',
+      payload: 5000
+    });
+  }
+}
