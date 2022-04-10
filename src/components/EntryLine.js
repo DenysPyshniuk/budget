f9ab75e78e3e0c0772cfa175021047ea2fe22910
Denys Pyshniuk <denys.pyshniuk@gmail.com>
2022-04-10 13:02:35 -0700

    Change style of entries

--- src/components/EntryLine.js
+++ src/components/EntryLine.js
@@ -9,7 +9,7 @@ function EntryLine({id, description, value, isExpense = false}) {
 
   return (
     <Fragment>
-        <Segment color={isExpense ? 'red' : 'green'}>
+        <Segment inverted color={isExpense ? 'red' : 'green'}>
             <Grid columns={3} textAlign='right'>
               <Grid.Row>
               <Grid.Column width={10} textAlign='left'>{description}</Grid.Column>
