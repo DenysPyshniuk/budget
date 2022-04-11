df06072d23c9a0b8c29520876c92d426fe985815
Denys Pyshniuk <denys.pyshniuk@gmail.com>
2022-04-10 23:00:25 -0700

    Create and use Add and Remove entries sagas

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,7 +1,11 @@
 // import * as testSaga from './testSaga';
 import * as entriesSaga from './entriesSaga';
+import * as entriesSagaDelete from './entriesSagaDelete';
+import * as entriesSagaAdd from './entriesSagaAdd';
 
 export function initSagas(sagaMiddleware) {
   // Object.values(testSaga).forEach(sagaMiddleware.run.bind(sagaMiddleware))
   Object.values(entriesSaga).forEach(sagaMiddleware.run.bind(sagaMiddleware))
+  Object.values(entriesSagaDelete).forEach(sagaMiddleware.run.bind(sagaMiddleware))
+  Object.values(entriesSagaAdd).forEach(sagaMiddleware.run.bind(sagaMiddleware))
 }
